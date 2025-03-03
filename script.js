45554a776e9878cad4a0510a6d814d5e4388c9f5
kartik132000 <sujeetkumarsen37@gmail.com>
2025-03-03 15:47:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// This is the boilerplate code given for you
-// You can modify this code
-
 const products = [
   { id: 1, name: "Product 1", price: 10 },
   { id: 2, name: "Product 2", price: 20 },
@@ -9,7 +6,6 @@ const products = [
   { id: 5, name: "Product 5", price: 50 },
 ];
 
-// DOM elements
 const productList = document.getElementById("product-list");
 const cartList = document.getElementById("cart-list");
 const clearCartBtn = document.getElementById("clear-cart-btn");
@@ -17,9 +13,10 @@ const clearCartBtn = document.getElementById("clear-cart-btn");
 let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
 function renderProducts() {
+  productList.innerHTML = "";
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = ${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>;
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
     productList.appendChild(li);
   });
 }
@@ -28,10 +25,9 @@ function renderCart() {
   cartList.innerHTML = "";
   cart.forEach((item) => {
     const li = document.createElement("li");
-    li.innerHTML = ${item.name} - $${item.price} <button class="remove-from-cart-btn" data-id="${item.id}">Remove</button>;
+    li.innerHTML = `${item.name} - $${item.price} <button class="remove-from-cart-btn" data-id="${item.id}">Remove</button>`;
     cartList.appendChild(li);
   });
-
   sessionStorage.setItem("cart", JSON.stringify(cart));
 }
 
